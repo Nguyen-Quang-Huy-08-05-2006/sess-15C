48884cd6fe415957dfcbfdac8dc7e86df4e9eb96
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-12-13 08:37:43 +0700

    Update btvn9Sess15.cpp

--- btvn9Sess15.cpp
+++ btvn9Sess15.cpp
@@ -3,14 +3,25 @@
 
 int main(){
 	char str[]="hello hola ole",text;
+	int length=strlen(str),count=0;
 	printf("%s",str);
 	printf("\nnhap ky tu muon xoa: ");
 	scanf("%c",&text);
-	for(int i=0;i<strlen(str);i++){
+	
+	for(int i=0;i<length;i++){
 		if(str[i]==text){
-			str[i]=' ';
+			for(int j=i;j<length;j++){
+				str[j+1]=str[j];
+			}
+		i--;
+		count++;
+		length--;
 		}
 	}
+	if(count==0){
+		printf("%d khong co trong chuoi");
+	}
+	
 	printf("mang sau khi bi xoa la: %s",str);
 	
 	return 0;
